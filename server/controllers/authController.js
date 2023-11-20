165ddd1959e678ae44df374e0c678b45a996a81f
sanjaymohansingh <sanjaysingh19p@gmail.com>
2023-11-20 06:53:00 -0800

    create sign in api route

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require("../models/userModel");
 const bcryptjs = require("bcryptjs");
 const errorHandler = require("../utils/error");
+const jwt = require("jsonwebtoken");
 
 const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -16,4 +17,28 @@ const signup = async (req, res, next) => {
   }
 };
 
-module.exports = signup;
+const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+    const token = jwt.sign(
+      { id: validUser._id },
+      process.env.ACCESS_TOKEN_SECRET
+    );
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("access_token", token, {
+        httpOnly: true,
+        expires: new Date(Date.now() + 24 * 60 * 60 * 1000),
+      })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+module.exports = { signup, signin };
