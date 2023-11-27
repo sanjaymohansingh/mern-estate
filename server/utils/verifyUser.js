79583b7acb4eb6136fae9a93ff4791209e22ee66
sanjaymohansingh <sanjaysingh19p@gmail.com>
2023-11-27 03:22:25 -0800

    complete update user functionality

--- server/utils/verifyUser.js
+++ server/utils/verifyUser.js
@@ -1,11 +1,12 @@
 const jwt = require("jsonwebtoken");
+const { errorHandler } = require("./error");
 
 const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
-  if (!token) return next(errorHandler(401, "Unauthorised"));
+  if (!token) return next(errorHandler(401, "Unauthorized"));
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return new (errorHandler(403, "Forbidden"))();
+    if (err) return next(errorHandler(403, "Forbidden"));
 
     req.user = user;
     next();
