088c03bba6e7b67dab87357184d5cec4c9e84415
sanjaymohansingh <sanjaysingh19p@gmail.com>
2023-12-02 02:49:33 -0800

    complete create listing page functionality

--- client/src/pages/CreateListing.jsx
+++ client/src/pages/CreateListing.jsx
@@ -21,7 +21,7 @@ export default function CreateListing() {
     type: "rent",
     bedrooms: 1,
     bathrooms: 1,
-    regularPrice: 50,
+    regularPrice: 500,
     discountPrice: 0,
     offer: false,
     parking: false,
