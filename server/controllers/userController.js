22b44f08e9ae4b3093f829f2bd0db1845b5b26ff
sanjaymohansingh <sanjaysingh19p@gmail.com>
2023-12-02 04:29:11 -0800

    create get user listings api route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,7 @@
 const bcryptjs = require("bcryptjs");
 const User = require("../models/userModel");
 const { errorHandler } = require("../utils/error");
+const Listing = require("../models/listingModel");
 
 const test = (req, res) => {
   res.json({
@@ -49,4 +50,17 @@ const deleteUser = async (req, res, next) => {
   }
 };
 
-module.exports = { test, updateUser, deleteUser };
+const getUserListings = async (req, res, next) => {
+  if (req.user.id === req.params.id) {
+    try {
+      const listings = await Listing.find({ userRef: req.params.id });
+      res.status(200).json(listings);
+    } catch (error) {
+      next(error);
+    }
+  } else {
+    return next(errorHandler(401, "You can only view your own listings!"));
+  }
+};
+
+module.exports = { test, updateUser, deleteUser, getUserListings };
--- server/routes/userRoute.js
+++ server/routes/userRoute.js
@@ -3,12 +3,14 @@ const {
   test,
   updateUser,
   deleteUser,
+  getUserListings,
 } = require("../controllers/userController");
 const verifyToken = require("../utils/verifyUser");
 
 const router = express.Router();
 
 router.get("/test", test);
+router.get("/listings/:id", verifyToken, getUserListings);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
 
