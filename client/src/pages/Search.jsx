eb5efe893ecb32e9d828492a51d5a82ec0b0ce3d
sanjaymohansingh <sanjaysingh19p@gmail.com>
2023-12-14 06:08:54 -0800

    complete header search form functionality

---
+++ client/src/pages/Search.jsx
@@ -0,0 +1,14 @@
+const Search = () => {
+  return (
+    <div>
+      <div className="">
+        <form></form>
+      </div>
+      <div className="">
+        <h1></h1>
+      </div>
+    </div>
+  );
+};
+
+export default Search;
